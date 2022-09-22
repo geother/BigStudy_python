0fd3a5069b632eedfa6e1f7e716a546dc30c24f2
geother <geother@foxmail.com>
2022-09-22 15:23:05 +0800

    add action

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-"""
-@author:yuzai
-@file:main.py
-@time:2022/09/17
-"""
-from Qndxx import Qndxx
+import json
+import re
+import requests
+import os
+from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    laravel_session = "BO9IapjHqEMceAN1azaq1jlmWCrjNRbil371NLKL"
-    qndxx = Qndxx(laravel_session)
-    qndxx.login()
-    qndxx.confirm()
+laravelSession = os.getenv('cookie')
+UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN"
+loginUrl = "https://service.jiangsugqt.org/youth/lesson"
+confirmUrl = "https://service.jiangsugqt.org/youth/lesson/confirm"
+session = requests.session()
+userinfo = {}
+
+# GET
+params = {
+    "s": "/youth/lesson",
+    "form": "inglemessage",
+    "isappinstalled": "0"
+}
+headers = {
+    'User-Agent': UA,
+    'Cookie': "laravelSession=" + laravelSession
+}
+getRes = session.get(url=loginUrl, headers=headers, params=params)
+
+if '抱歉，出错了' in getRes.text:
+    print("laravel_session错误")
+    raise Exception("GET RE")
+
+
+userinfo['token'] = re.findall(r'var token ?= ?"(.*?)"', getRes.text)[0]
+userinfo['lesson_id'] = re.findall(r"'lesson_id':(.*)", getRes.text)[0]
+
+temp = BeautifulSoup(getRes.text, 'html.parser').select(".confirm-user-info p")
+for i in temp:
+    item = BeautifulSoup(str(i), 'html.parser').get_text()
+    userinfo[item[:4]] = item[5:]
+
+params = {
+    "_token": userinfo.get('token'),
+    "lesson_id": userinfo.get('lesson_id')
+}
+
+# POST
+res = json.loads(session.post(url=confirmUrl, params=params).text)
+
+if res["status"] == 1 and res["message"] == "操作成功":
+    print("江苏青年大学习已完成")
+    print(f"您的信息:{userinfo}")
+else:
+    raise Exception("POST RE")
