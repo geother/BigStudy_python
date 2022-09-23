564b59de17614b4607857f44651a5e869fe666dd
focuseds <h3ureux@gmail.com>
2022-09-23 15:21:47 +0800

    Add func 通过server酱发送至微信

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from bs4 import BeautifulSoup
 
 laravel_session = os.environ["COOKIE"]
+SEND_KEY = os.environ["SEND_KEY"]
 
 UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x18001234) NetType/WIFI Language/zh_CN"
 loginUrl = "https://service.jiangsugqt.org/youth/lesson"
@@ -21,6 +22,20 @@ headers = {
 'User-Agent': UA,
 'Cookie': "laravel_session=" + laravel_session  # 抓包获取
 }
+
+def to_wechat(session):
+    """
+    学习完成后通过server酱发送至微信，附带学习成绩报告的链接
+    :param session:
+    :return:
+    """
+    data = {
+        'title': '青年大学习已完成',
+        'desp': 'https://service.jiangsugqt.org/youth/report'
+    }
+    server_chan = "https://sctapi.ftqq.com/{}.send".format(SEND_KEY)
+    resp = session.get(server_chan, headers=headers, params=data)
+    print('Server酱:', resp.text)
 
 # GET
 getRes = session.get(url=loginUrl, headers=headers, params=params)
@@ -45,5 +60,6 @@ print(f"返回结果:{res}")
 if res["status"] == 1 and res["message"] == "操作成功":
     print("青年大学习已完成")
     print(f"您的信息:{userinfo}")
+    to_wechat(session)
 else:
     raise Exception("POST 错误")
